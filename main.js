4630ac88a4058e77805f1c3c550caf3fe85a5555
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-29 17:58:58 +0530

    multiple users added and listOfUsers displayed on the screen

--- main.js
+++ main.js
@@ -14,7 +14,23 @@ function addUser(e){
   }
   //serialized the object and convert it into string
   const userDetails_serialized = JSON.stringify(userDetails);
-  localStorage.setItem('userDetails',userDetails_serialized);
+  localStorage.setItem(email,userDetails_serialized);
   //deserializing
-  console.log(JSON.parse(localStorage.getItem('userDetails')));
+  //console.log(JSON.parse(localStorage.getItem('userDetails')));
+
+  addElement(userDetails);
+}
+
+Object.keys(localStorage).forEach((key)=>{
+  const obj =JSON.parse(localStorage.getItem(key));
+
+  addElement(obj);
+});
+
+function addElement(obj){
+  const ul = document.querySelector('#users');
+  const new_li = document.createElement('li');
+  new_li.className = "item";
+  new_li.appendChild(document.createTextNode(`${obj.name} : ${obj.email}`));
+  ul.appendChild(new_li);
 }
