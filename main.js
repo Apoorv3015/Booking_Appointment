1cf8c172fe1aa014c3a00721b4bc925ec0529f78
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-29 15:53:18 +0530

    object added to localstorage

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ function addUser(e){
   e.preventDefault();
   const name = e.target.name.value;
   const email = e.target.email.value;
-  localStorage.setItem('name',name);
-  localStorage.setItem('email',email);  
+  // localStorage.setItem('name',name);
+  // localStorage.setItem('email',email); 
+  const userDetails = {
+    name:name,
+    email : email
+  }
+  //serialized the object and convert it into string
+  const userDetails_serialized = JSON.stringify(userDetails);
+  localStorage.setItem('userDetails',userDetails_serialized);
+  //deserializing
+  console.log(JSON.parse(localStorage.getItem('userDetails')));
 }
